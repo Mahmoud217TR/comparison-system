46726e068843017d11575772cde1b4bc824438d8
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-21 20:06:39 +0300

    accessors & mutators

--- app/Models/Comparison.php
+++ app/Models/Comparison.php
@@ -2,12 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comparison extends Model
 {
     use HasFactory;
+
+    public static function states(){
+        return [
+            1 => 'private',
+            2 => 'public',
+        ];
+    }
+
+    protected $attributes = [
+        'state' => 1,
+    ];
+    
+    // The Accessor & Mutator
+    public function state(): Attribute{
+        return Attribute::make(
+            get: fn ($value) => $this->states()[$value],
+            set: fn ($value) => array_search($value,$this->states()),
+        );
+    }
 
     public function items(){
         return $this->hasMany(Item::class);
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,12 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
     use HasFactory;
+
+    public static function types(){
+        return [
+            1 => 'string',
+            2 => 'numeric',
+        ];
+    }
+
+    protected $attributes = [
+        'type' => 1,
+    ];
+    
+    // The Accessor & Mutator
+    public function type(): Attribute{
+        return Attribute::make(
+            get: fn ($value) => $this->types()[$value],
+            set: fn ($value) => array_search($value,$this->types()),
+        );
+    }
 
     public function comparison(){
         return $this->belongsTo(Comparison::class);
