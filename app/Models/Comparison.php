6b72457e6e571ec587b71f1c4182397e50dd90ea
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-21 20:01:54 +0300

    relations

--- app/Models/Comparison.php
+++ app/Models/Comparison.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Comparison extends Model
 {
     use HasFactory;
+
+    public function items(){
+        return $this->hasMany(Item::class);
+    }
+
+    public function types(){
+        return $this->hasMany(Type::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    public function comparison(){
+        return $this->belongsTo(Comparison::class);
+    }
+
+    public function fields(){
+        return $this->belongsToMany(Type::class)->withPivot('value');
+    }
 }
