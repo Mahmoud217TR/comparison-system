29f1bac6f827f91a0c338b6c90f00a33d8efc829
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-06-24 20:21:29 +0300

    index

--- resources/views/comparison/index.blade.php
+++ resources/views/comparison/index.blade.php
@@ -3,7 +3,42 @@
 @section('title',"Your Comparisons")
 
 @section('content')
-    @foreach ($comparisons as $comparison)
-        
-    @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    <tr>
+                        <th>Comparison</th>
+                        <th>Items</th>
+                        <th>Show</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    @foreach ($comparisons as $comparison)
+                    <tr>
+                        <th>
+                            {{ $comparison->title }}
+                        </th>
+                        <th>
+                            {{ $comparison->items->count() }}
+                        </th>
+                        <th>
+                            <a href="{{ route('comparison.show', $comparison) }}" class="btn btn-sm btn-primary">Show</a>
+                        </th>
+                        <th>
+                            <a href="{{ route('comparison.edit', $comparison) }}" class="btn btn-sm btn-success">Edit</a>
+                        </th>
+                        <th>
+                            <form action="{{ route('comparison.destroy', $comparison) }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button class="btn btn-sm btn-danger">Delete</button>
+                            </form>
+                        </th>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
